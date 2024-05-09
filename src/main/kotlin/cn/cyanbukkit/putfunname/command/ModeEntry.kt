c57ecc8a621f09abc4b88adb1f3fb25f2bc86b91
SmallXY <62281180+SmallXY@users.noreply.github.com>
2024-05-09 23:15:32 +0800

    Update ModeEntry.kt

--- src/main/kotlin/cn/cyanbukkit/putfunname/command/ModeEntry.kt
+++ src/main/kotlin/cn/cyanbukkit/putfunname/command/ModeEntry.kt
@@ -38,7 +38,7 @@ object ModeEntry : Command("mode") {
         p: Player
     ): Array<Any> {
         val convertedArgs = Array<Any>(info.size) { i ->
-            when (parameterTypes[i + 1]) { // i + 1 because the first parameter is Player
+            when (parameterTypes[i]) { // i + 1 because the first parameter is Player
                 Double::class.java -> info[i].toDouble()
                 Int::class.java -> info[i].toInt()
                 String::class.java -> info[i]
